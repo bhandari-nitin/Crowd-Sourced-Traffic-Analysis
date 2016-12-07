9eac118cb533faf2cc3004462cc8f6ccb7d20229
Belhadjmo <belhadjmo75@gmail.com>
2016-12-06 20:47:25 -0600

    Add files via upload

---
+++ Mohammed/Source/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.mohammed.wirelesspianoapp;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mysound;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mysound = MediaPlayer.create(this, R.raw.a4);
+
+    }
+
+    public void playMusic(View view) {
+        mysound.start();
+    }
+}
---
+++ Mohammed/Source/PianoLogActivity.java
@@ -0,0 +1,13 @@
+package com.example.mohammed.pianoapplogreg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PianoLogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_piano_log);
+    }
+}
